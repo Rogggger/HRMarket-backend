a25dd7ca110a22c38754b31a5051866e3efc5d4f
root <root@yuanw.wang>
2018-03-25 22:44:54 +0800

    删除config和daemonrun的链接

--- daemonrun.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This is our first script.
-source ../venv/bin/activate
-echo 'entering venv...\n'
-gunicorn -w 3 startserver:application -b unix:/tmp/gunicorn.sock -D 
-echo 'Starting server successfully\n'
