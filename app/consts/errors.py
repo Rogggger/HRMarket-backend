518f73b3bb77949325443a36508566f273b5bc03
wang yuanwei <wangyuanwei@WangYuanwei-01.local>
2018-03-26 11:09:14 +0800

    增加README

--- app/consts/errors.py
+++ app/consts/errors.py
@@ -1,11 +1,14 @@
 # coding: utf-8
 ERROR_MAP = {
+    # system error
     10000001: u"参数不正确",
+    10000002: u"服务器内部错误",
+    # account error
     10000011: u"账户已经存在",
     10000012: u"账户不存在",
     10000013: u"密码不正确",
-    10000014: u"不能取得信息",
+    # info error
+    # data error
     # functional error code:
-    10001001: u"Sync error",
 
 }
