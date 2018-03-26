cd0e2fdb8d07872603fc0b73ff960046b0769e8f
wang yuanwei <wangyuanwei@WangYuanwei-01.local>
2018-03-26 11:19:28 +0800

    完成基础的数据提交和查询功能

--- app/view/user/data_record.py
+++ app/view/user/data_record.py
@@ -1,32 +1,17 @@
 #  coding: utf-8
+import datetime
+
 from flask_login import login_required, current_user
-from app.model.data_collection import DataCollection
+
 from flask import Blueprint, request
-from marshmallow import Schema, fields
+
+from app.model.data_collection import DataCollection
 from app.libs.http import jsonify, error_jsonify
 from app.libs.db import session
-import datetime
-from app.consts import (
-    InvalidArguments,
-    GetInfoError
-)
+from app.serializer.data import DataParaSchema
 
 # 企业数据填报
-
 bp_data = Blueprint("data", __name__, url_prefix="/data")
-
-
-class DataParaSchema(Schema):
-    filing = fields.Integer(10)  # 初次建档时就业人数
-    check = fields.Integer(10)  # 本次调查期就业人数
-    other_reason = fields.String(55)  # 其他原因
-    decrease_type = fields.String(50)  # 就业人数减少类型
-    main_reason = fields.String(50)  # 主要原因
-    main_reason_detail = fields.String(100)  # 主要原因说明
-    second_reason = fields.String(50)  # 次要原因
-    second_reason_detail = fields.String(100)  # 次要原因说明
-    third_reason = fields.String(50)  # 第三原因
-    third_reason_detail = fields.String(100)  # 第三原因
 
 
 @bp_data.route("/record", methods=['POST'])
@@ -39,13 +24,17 @@ def info_record():
         return error_jsonify(10000001, errors)
     else:
         now = datetime.datetime.now()
-        now = now.strftime("%Y-%m-%d %H:%M:%S")
         data['time'] = now
         data['status'] = 0
         data['user_id'] = current_user.id
-        new_data = DataCollection(**data)
-        session.add(new_data)
-        session.commit()
+        try:
+            new_data = DataCollection(**data)
+            session.add(new_data)
+        except Exception:
+            session.rollback()
+            return error_jsonify(10000002)
+        else:
+            session.commit()
         return jsonify({})
 
 
@@ -53,9 +42,10 @@ def info_record():
 @login_required
 def info_get():
     cur_id = current_user.id
-    if DataCollection.is_exist(cur_id):
-        tmp_data = DataCollection.query.filter_by(user_id=cur_id).first()
-        json, error = DataParaSchema().dump(tmp_data)
-        return jsonify(json)
-    else:
-        return error_jsonify(GetInfoError, specifiy_error="Can not get the info", status_code=400)
+    tmp_data = DataCollection.query.filter_by(user_id=cur_id).first()
+    if tmp_data is None:
+        tmp_data = {}
+    json, error = DataParaSchema().dump(tmp_data)
+    if error:
+        return error_jsonify(10000002)
+    return jsonify(json)
