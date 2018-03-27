70a1e312c4d20642d5b60343ccff42af34386675
victor <1103464147@qq.com>
2018-03-27 11:31:17 +0800

    修改企业填报信息相关接口

--- app/view/user/data_record.py
+++ app/view/user/data_record.py
@@ -2,7 +2,7 @@
 import datetime
 
 from flask_login import login_required, current_user
-
+from sqlalchemy import and_
 from flask import Blueprint, request
 from marshmallow import Schema, fields
 
@@ -30,51 +30,47 @@ def info_record():
 
     if errors:
         return error_jsonify(10000001, errors)
-    else:
-        now = datetime.datetime.now()
-        data['time'] = now
-        can_find_time_range = 0  # 能否确定时间段标志
-        admin_user = User.query.filter_by(isAdmin=2).first()  # 找到省管理员账户id
-        report_time = ReportTime.query.filter_by(user_id=admin_user.id).all()  # 找到省级管理员设置的提交时间
-        for tmp_time in report_time:
-            if tmp_time.start_time <= now <= tmp_time.end_time:  # 确定当前时间的时间段,默认只有一个符合
-                can_find_time_range = 1
-                tmp_data = DataCollection.query.filter_by(user_id=current_user.id, time_id=tmp_time.id)
-                # 找到企业填报的符合条件的数据
-                if tmp_data:  # 修改企业条目，并保存
-                    tmp_data.update(data)
-                    session.commit()
-                    break
-                else:  # 新建一个企业填报条目，并保存
-                    data['time_id'] = tmp_time.id
-                    data['status'] = 0
-                    data['user_id'] = current_user.id
-                    new_data = DataCollection(**data)
-                    session.add(new_data)
-                    session.commit()
-                    break
-        if can_find_time_range == 0:  # 现在不在任何可以填报的时间段内
-            return error_jsonify(10000014)
-        return jsonify({})
+    now = datetime.datetime.now()
+    data['time'] = now
+    admin_user = User.query.filter_by(isAdmin=2).first()  # 找到省管理员账户id
+    report_time = ReportTime.query.filter(and_(ReportTime.user_id == admin_user.id, ReportTime.start_time <= now,
+                                               ReportTime.end_time >= now)).first()
+    # 找到省级管理员设置的提交时间中符合条件的
+    if report_time:  # 如果在当前时间段
+        tmp_data = DataCollection.query.filter_by(user_id=current_user.id, time_id=report_time.id)
+        # 找到企业填报的符合条件的数据
+        if tmp_data.first():  # 修改企业条目，并保存
+            if tmp_data.first().status != 0:
+                return error_jsonify(10000016)
+            tmp_data.update(data)
+            session.commit()
+        else:  # 新建一个企业填报条目，并保存
+            data['time_id'] = report_time.id
+            data['status'] = 0
+            data['user_id'] = current_user.id
+            new_data = DataCollection(**data)
+            session.add(new_data)
+            session.commit()
+    else:  # 现在不在任何可以填报的时间段内
+        return error_jsonify(10000014)
+    return jsonify({})
 
 
 @bp_data.route("/record", methods=['GET'])
 @login_required
 def info_record_get():
     now = datetime.datetime.now()
-    can_find_time_range = 0  # 能否确定时间段标志
     admin_user = User.query.filter_by(isAdmin=2).first()  # 找到省管理员账户id
-    report_time = ReportTime.query.filter_by(user_id=admin_user.id).all()  # 找到省级管理员设置的提交时间
-    for tmp_time in report_time:
-        if tmp_time.start_time <= now <= tmp_time.end_time:  # 确定当前时间的时间段,默认只有一个符合
-            can_find_time_range = 1
-            tmp_data = DataCollection.query.filter_by(user_id=current_user.id, time_id=tmp_time.id).first()
-            # 找到企业填报的符合条件的数据
-            if tmp_data:
-                data_need, errors = DataParaSchema().dump(tmp_data)
-                return jsonify(data_need)
-    if can_find_time_range == 0:  # 现在不在任何可以填报的时间段内
-        return error_jsonify(10000014)
+    report_time = ReportTime.query.filter(and_(ReportTime.user_id == admin_user.id, ReportTime.start_time <= now,
+                                               ReportTime.end_time >= now)).first()
+    if report_time:
+        tmp_data = DataCollection.query.filter_by(user_id=current_user.id, time_id=report_time.id).first()
+        # 找到企业填报的符合条件的数据
+        if tmp_data:
+            data_need, errors = DataParaSchema().dump(tmp_data)
+            return jsonify(data_need)
+    else:
+        return jsonify({})
 
 
 @bp_data.route("/report", methods=['POST'])
@@ -82,29 +78,29 @@ def info_record_get():
 def info_report():
     json = request.get_json()
     data, errors = DataParaSchema().load(json)
+
     if errors:
         return error_jsonify(10000001, errors)
-    else:
-        now = datetime.datetime.now()
-        data['time'] = now
-        can_find_time_range = 0  # 能否确定时间段标志
-        admin_user = User.query.filter_by(isAdmin=2).first()  # 找到省管理员账户id
-        report_time = ReportTime.query.filter_by(user_id=admin_user.id).all()  # 找到省级管理员设置的提交时间
-        for tmp_time in report_time:
-            if tmp_time.start_time <= now <= tmp_time.end_time:  # 确定当前时间的时间段,默认只有一个符合
-                can_find_time_range = 1
-                tmp_data = DataCollection.query.filter_by(user_id=current_user.id, time_id=tmp_time.id)
-                # 找到企业填报的符合条件的数据
-                if tmp_data:  # 修改企业条目，并上报
-                    data['status'] = 1
-                    tmp_data.update(data)
-                    session.commit()
-                    break
-                else:  # 没有找到，说明信息没有保存
-                    return error_jsonify(10000015)
-        if can_find_time_range == 0:  # 现在不在任何可以填报的时间段内
-            return error_jsonify(10000014)
-        return jsonify({})
+    now = datetime.datetime.now()
+    data['time'] = now
+    admin_user = User.query.filter_by(isAdmin=2).first()  # 找到省管理员账户id
+    report_time = ReportTime.query.filter(and_(ReportTime.user_id == admin_user.id, ReportTime.start_time <= now,
+                                               ReportTime.end_time >= now)).first()
+    # 找到省级管理员设置的提交时间中符合条件的
+    if report_time:  # 如果在当前时间段
+        tmp_data = DataCollection.query.filter_by(user_id=current_user.id, time_id=report_time.id)
+        # 找到企业填报的符合条件的数据
+        if tmp_data:  # 上报
+            if tmp_data.first().status != 0:
+                return error_jsonify(10000016)
+            data['status'] = 1
+            tmp_data.update(data)
+            session.commit()
+        else:  # 没有找到说明没有保存
+            return error_jsonify(10000015)
+    else:  # 现在不在任何可以填报的时间段内
+        return error_jsonify(10000014)
+    return jsonify({})
 
 
 @bp_data.route("/get", methods=['GET', 'POST'])
