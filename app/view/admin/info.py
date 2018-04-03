596f6ceeac0c66363ef3f24c9715441643f17793
wang yuanwei <wangyuanwei@WangYuanwei-01.local>
2018-04-03 11:00:04 +0800

    增加文档

--- app/view/admin/info.py
+++ app/view/admin/info.py
@@ -1,11 +1,12 @@
 #  coding: utf-8
 
-from flask_login import login_required
+from flask_login import login_required, current_user
 from flask import request, Blueprint
 from app.model.corporate_Info import Info
 from app.serializer.info import InfoParaSchema, InfoSearchSchema
 from app.decorator.auth import admin_required
 from app.libs.http import error_jsonify, jsonify
+from app.consts.user import PERMISSION_CITY
 
 # 数据汇总
 bp_admin_info = Blueprint('admin_info', __name__, url_prefix='/admin/info')
@@ -20,6 +21,8 @@ def get_info():
     if errors:
         return error_jsonify(10000001, errors)
     area = u'%{}%'.format(data.get('area'))
-    infos = Info.query.filter(Info.area.like(area)).all()
-    json = InfoParaSchema(many=True).dump(infos)
-    return jsonify(json)
+    infos = Info.query.filter(Info.area.like(area))
+    if current_user.isAdmin == PERMISSION_CITY:
+        infos.filter(Info.area.like(u'%{}%'.format(current_user.area)))
+    json = InfoParaSchema(many=True).dump(infos.all())
+    return jsonify(json.data)
